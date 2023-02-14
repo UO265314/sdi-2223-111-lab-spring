539fa36e88aaaf9c52b207165d87cdcf7a45bc7e
Pablo <uo265314@uniovi.es>
2023-02-14 20:52:54 +0100

    SDE-2223-111-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/notaineitor/Sdi2223111SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.notaineitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223111SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223111SpringApplication.class, args);
+    }
+
+}
